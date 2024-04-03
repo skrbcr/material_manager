45259c4bb82773689ce1efcc7dd1d6ae00179c63
skrbcr <90087433+skrbcr@users.noreply.github.com>
2024-04-03 16:58:01 +0900

    Formated

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # 素材リスト
-動画などで使う素材の規約などを一覧にまとめて表示するWebアプリケーション的なものです。ニコニコの親登録で便利な、使用した作品の番号をまとめて出力する機能も。
+
+動画などで使う素材の規約などを一覧にまとめて表示する Web アプリケーション的なものです。ニコニコの親登録で便利な、使用した作品の番号をまとめて出力する機能も。
 
 ## 機能
+
 動画などで使う素材をリストアップ、リストは保存することができます。
 
 ![使用例1](./img/1.png)
@@ -19,18 +21,22 @@ sm000000 im000000
 詳細は [マニュアル](./manual.md) をご覧ください。
 
 ## インストール＆使い方
+
 ### インターネット上で使う
+
 1. [こちら](https://skrbcr.github.io/material_manager/) にアクセス
 
 ### ローカルにダウンロードして使う
+
 1. [こちら](https://github.com/skrbcr/material_manager/archive/refs/heads/main.zip) からダウンロード、展開。
 2. `index.html` をブラウザ（Google Chrome, Microsoft Edge など）で開く
 
 ## 注意！
+
 1. 動作保証は Google Chrome, Microsoft Edge のみです。Firefox などでは機能しない可能性が高いです（[詳細](https://developer.mozilla.org/ja/docs/Web/API/Window/showSaveFilePicker#%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%83%BC%E3%81%AE%E4%BA%92%E6%8F%9B%E6%80%A7)）。
 2. ライセンスは MIT License です。そのため無保証となります。
 3. ライブラリを読み込むためにインターネットに接続されている必要があります。
 
 ## Special Thanks
-高機能な表は [Tabulator](https://tabulator.info/) を用いて実現しています。絵文字に [Font Awesome](https://fontawesome.com/) を利用しています。
 
+高機能な表は [Tabulator](https://tabulator.info/) を用いて実現しています。絵文字に [Font Awesome](https://fontawesome.com/) を利用しています。
--- script.js
+++ script.js
@@ -2,207 +2,317 @@ let maxId = 1;
 let bChanged = false;
 
 function link_url(cell, formatterParams) {
-	let url = cell.getValue();
-	return "<a class=\"hlink_table\" href=\"" + url + "\" target=\"_blank\">" + url + "</a>";
+  let url = cell.getValue();
+  return (
+    '<a class="hlink_table" href="' + url + '" target="_blank">' + url + "</a>"
+  );
 }
 
 function cell_edited(cell) {
-	bChanged = true;
+  bChanged = true;
 }
 
 function alert_unsave(e) {
-	if (bChanged) {
-		e.preventDefault();
-		e.returnValue = "";
-	}
+  if (bChanged) {
+    e.preventDefault();
+    e.returnValue = "";
+  }
 }
 
-let table_init = [
-    { id: 1, url: "" },
-];
+let table_init = [{ id: 1, url: "" }];
 
-let headerMenu = function() {
-    let menu = [];
-    let columns = this.getColumns();
+let headerMenu = function () {
+  let menu = [];
+  let columns = this.getColumns();
 
-    for (let column of columns) {
-        if (column.getDefinition().title == "selection") continue;
-        let icon = document.createElement("i");
-        icon.classList.add("fas");
-        icon.classList.add(column.isVisible() ? "fa-check-square" : "fa-square");
-        let label = document.createElement("span");
-        let title = document.createElement("span");
-        title.textContent = " " + column.getDefinition().title;
-        label.appendChild(icon);
-        label.appendChild(title);
+  for (let column of columns) {
+    if (column.getDefinition().title == "selection") continue;
+    let icon = document.createElement("i");
+    icon.classList.add("fas");
+    icon.classList.add(column.isVisible() ? "fa-check-square" : "fa-square");
+    let label = document.createElement("span");
+    let title = document.createElement("span");
+    title.textContent = " " + column.getDefinition().title;
+    label.appendChild(icon);
+    label.appendChild(title);
 
-        menu.push({
-            label: label,
-            action: function(e) {
-                e.stopPropagation();
-                column.toggle();
-                if (column.isVisible()) {
-                    icon.classList.remove("fa-square");
-                    icon.classList.add("fa-check-square");
-                }
-                else {
-                    icon.classList.remove("fa-check-square");
-                    icon.classList.add("fa-square");
-                }
-            },
-        });
-    }
-    return menu;
+    menu.push({
+      label: label,
+      action: function (e) {
+        e.stopPropagation();
+        column.toggle();
+        if (column.isVisible()) {
+          icon.classList.remove("fa-square");
+          icon.classList.add("fa-check-square");
+        } else {
+          icon.classList.remove("fa-check-square");
+          icon.classList.add("fa-square");
+        }
+      },
+    });
+  }
+  return menu;
 };
 
 let table = new Tabulator("#main-table", {
-	data: table_init,
-	// layout: "fitColumns",
-	// responsiveLayout: "hide",
-	height: 450,
-	// maxHeight: "100%",
-	addRowPos: "top",
-	history: true,
-	movableColumns: true,
-	initialSort: [
-		{ column: "author", dir: "asc"},
-		{ column: "type", dir: "asc" },
-	],
-	columnDefaults: {
-		tooltip: true,
-	},
-	groupBy: "type",
-	
-	// autoColumns: true,
-	rowHeader: { title: "selection", headerSort: false, resizable: false, frozen: true, headerHozAlign: "center", hozAlign: "center", formatter: "rowSelection", titleFormatter: "rowSelection", cellClick:function(e, cell) {
-		cell.getRow().toggleSelect();
-	},
-        headerMenu: headerMenu, },
+  data: table_init,
+  // layout: "fitColumns",
+  // responsiveLayout: "hide",
+  height: 450,
+  // maxHeight: "100%",
+  addRowPos: "top",
+  history: true,
+  movableColumns: true,
+  initialSort: [
+    { column: "author", dir: "asc" },
+    { column: "type", dir: "asc" },
+  ],
+  columnDefaults: {
+    tooltip: true,
+  },
+  groupBy: "type",
+
+  // autoColumns: true,
+  rowHeader: {
+    title: "selection",
+    headerSort: false,
+    resizable: false,
+    frozen: true,
+    headerHozAlign: "center",
+    hozAlign: "center",
+    formatter: "rowSelection",
+    titleFormatter: "rowSelection",
+    cellClick: function (e, cell) {
+      cell.getRow().toggleSelect();
+    },
+    headerMenu: headerMenu,
+  },
   rowContextMenu: [
-		{
-			label: "素材を追加",
-			action: function(e, row) {
-                maxId += 1;
-                table.addRow({ id: maxId, url: "" });
-            }
-        },
+    {
+      label: "素材を追加",
+      action: function (e, row) {
+        maxId += 1;
+        table.addRow({ id: maxId, url: "" });
+      },
+    },
+    {
+      label: "素材を削除",
+      action: function (e, row) {
+        if (confirm("本当にこの素材を削除しますか？")) {
+          row.delete();
+        }
+      },
+    },
+  ],
+  columns: [
+    { title: "ID", width: 50, field: "id" },
+    {
+      title: "作者",
+      width: 90,
+      field: "author",
+      editor: "input",
+      cellEdited: cell_edited,
+    },
+    {
+      title: "作品名",
+      width: 200,
+      field: "name",
+      editor: "input",
+      cellEdited: cell_edited,
+    },
+    // { title: "作品名（詳細）", width: 135, field: "name_detail", editor: "input", cellEdited: cell_edited },
+    {
+      title: "種類",
+      width: 100,
+      field: "type",
+      editor: "input",
+      cellEdited: cell_edited,
+    },
+    {
+      title: "URL",
+      width: 200,
+      field: "url",
+      formatter: link_url,
+      editor: "input",
+      cellEdited: cell_edited,
+    },
+    {
+      title: "ニコニコ作品番号",
+      width: 100,
+      field: "niconico",
+      editor: "input",
+      cellEdited: cell_edited,
+    },
+    {
+      title: "ニコニコ利用",
+      width: 50,
+      field: "available_nico",
+      formatter: "tickCross",
+      sorter: "boolean",
+      editor: true,
+      cellEdited: cell_edited,
+    },
+    {
+      title: "YouTube 利用",
+      width: 50,
+      field: "available_yt",
+      formatter: "tickCross",
+      sorter: "boolean",
+      editor: true,
+      cellEdited: cell_edited,
+    },
+    {
+      title: "その他利用",
+      width: 50,
+      field: "available_others",
+      formatter: "tickCross",
+      sorter: "boolean",
+      editor: true,
+      cellEdited: cell_edited,
+    },
+    {
+      title: "商用利用",
+      width: 50,
+      field: "available_commerce",
+      formatter: "tickCross",
+      sorter: "boolean",
+      editor: true,
+      cellEdited: cell_edited,
+    },
+    {
+      title: "改変",
+      width: 50,
+      field: "available_mod",
+      formatter: "tickCross",
+      sorter: "boolean",
+      editor: true,
+      cellEdited: cell_edited,
+    },
+    {
+      title: "その他",
+      width: 150,
+      field: "others",
+      editor: "input",
+      cellEdited: cell_edited,
+    },
+  ],
+});
+
+table.on("rowSelectionChanged", function (data, rows, selected, deselected) {
+  bChanged = true;
+});
+
+document
+  .getElementById("output-nico")
+  .addEventListener("click", async function () {
+    let selectedRows = table.getSelectedData();
+    let strOut = "";
+    for (const element of selectedRows) {
+      if (element.niconico != "") {
+        strOut += element.niconico + " ";
+      }
+    }
+    const handle = await window.showSaveFilePicker({
+      suggestedName: "親作品番号",
+      types: [
         {
-            label: "素材を削除",
-            action: function(e, row) {
-			  if (confirm("本当にこの素材を削除しますか？")) {
-                    row.delete();
-                }
-            }
+          description: "Text file",
+          accept: { "text/plain": [".txt"] },
         },
-    ],
-	columns: [
-		{ title: "ID", width: 50, field: "id" },
-		{ title: "作者", width: 90, field: "author", editor: "input", cellEdited: cell_edited },
-		{ title: "作品名", width: 200, field: "name", editor: "input", cellEdited: cell_edited },
-		// { title: "作品名（詳細）", width: 135, field: "name_detail", editor: "input", cellEdited: cell_edited },
-		{ title: "種類", width: 100, field: "type", editor: "input", cellEdited: cell_edited },
-		{ title: "URL", width: 200, field: "url", formatter: link_url, editor: "input", cellEdited: cell_edited },
-		{ title: "ニコニコ作品番号", width: 100, field: "niconico", editor: "input", cellEdited: cell_edited },
-		{ title: "ニコニコ利用", width: 50, field: "available_nico", formatter: "tickCross", sorter: "boolean", editor: true, cellEdited: cell_edited },
-		{ title: "YouTube 利用", width: 50, field: "available_yt", formatter: "tickCross", sorter: "boolean", editor: true, cellEdited: cell_edited },
-		{ title: "その他利用", width: 50, field: "available_others", formatter: "tickCross", sorter: "boolean", editor: true, cellEdited: cell_edited },
-        { title: "商用利用", width: 50, field: "available_commerce", formatter: "tickCross", sorter: "boolean", editor: true, cellEdited: cell_edited },
-		{ title: "改変", width: 50, field: "available_mod", formatter: "tickCross", sorter: "boolean", editor: true, cellEdited: cell_edited },
-		{ title: "その他", width: 150, field: "others", editor: "input", cellEdited: cell_edited },
-	]
-});
+      ],
+    });
+    const writable = await handle.createWritable();
+    await writable.write(strOut);
+    await writable.close();
+  });
 
-table.on("rowSelectionChanged", function(data, rows, selected, deselected) {
-	bChanged = true;
-});
+document
+  .getElementById("save-project")
+  .addEventListener("click", async function () {
+    let selectedRows = table.getSelectedData();
+    let selectedRowIndices = [];
+    for (const element of selectedRows) {
+      selectedRowIndices.push(element.id);
+    }
+    let jsonOut = table.getData();
+    jsonOut.forEach((row) => {
+      row.select = selectedRowIndices.includes(row.id);
+    });
+    const pickerOpts = {
+      suggestedName: "list",
+      types: [
+        {
+          description: "JSON files",
+          accept: { "application/json": [".json"] },
+        },
+      ],
+    };
+    const handle = await window.showSaveFilePicker(pickerOpts);
+    const writable = await handle.createWritable();
+    await writable.write(JSON.stringify(jsonOut, null, 2));
+    await writable.close();
+    bChanged = false;
+  });
 
-document.getElementById("output-nico").addEventListener("click", async function() {
-	let selectedRows = table.getSelectedData();
-	let strOut = "";
-	for (const element of selectedRows) {
-		if (element.niconico != "") {
-			strOut += element.niconico + " ";
-		}
-	}
-	const handle = await window.showSaveFilePicker({
-		suggestedName: "親作品番号",
-		types: [{
-			description: "Text file",
-			accept: {"text/plain": [".txt"]},
-		}],
-	});
-	const writable = await handle.createWritable();
-	await writable.write(strOut);
-	await writable.close();
-});
+document
+  .getElementById("open-project")
+  .addEventListener("click", async function () {
+    if (
+      bChanged &&
+      !confirm("未保存の内容があります。放棄してファイルを開きますか？")
+    ) {
+      return;
+    }
+    const pickerOpts = {
+      types: [
+        {
+          description: "JSON files",
+          accept: { "application/json": [".json"] },
+        },
+      ],
+      excludeAcceptAllOption: true,
+      multiple: false,
+    };
+    let [fileHandle] = await window.showOpenFilePicker(pickerOpts);
+    const file = await fileHandle.getFile();
+    const fileContent = await file.text();
+    const tableData = JSON.parse(fileContent);
+    table.setData(tableData);
+    for (const element of tableData) {
+      if (element.select) {
+        table.selectRow(element.id);
+      }
+    }
+    maxId = Math.max(...tableData.map((row) => row.id));
+    bChanged = false;
+  });
 
-document.getElementById("save-project").addEventListener("click", async function() {
-	let selectedRows = table.getSelectedData();
-	let selectedRowIndices = [];
-	for (const element of selectedRows) {
-		selectedRowIndices.push(element.id);
-	}
-	let jsonOut = table.getData();
-	jsonOut.forEach(row => {
-		row.select = selectedRowIndices.includes(row.id);
-	});
-	const pickerOpts = {
-		suggestedName: "list",
-		types: [{
-			description: "JSON files",
-			accept: { "application/json": [ ".json" ] },
-		}],
-	}
-	const handle = await window.showSaveFilePicker(pickerOpts);
-	const writable = await handle.createWritable();
-	await writable.write(JSON.stringify(jsonOut, null, 2));
-	await writable.close();
-	bChanged = false;
-});
-
-document.getElementById("open-project").addEventListener("click", async function() {
-	if (bChanged && !confirm("未保存の内容があります。放棄してファイルを開きますか？")) {
-		return;
-	}
-	const pickerOpts = {
-		types: [{
-			description: "JSON files",
-			accept: { "application/json": [ ".json" ] },
-		}],
-		excludeAcceptAllOption: true,
-		multiple: false,
-	};
-	let [fileHandle] = await window.showOpenFilePicker(pickerOpts);
-	const file = await fileHandle.getFile();
-	const fileContent = await file.text();
-	const tableData = JSON.parse(fileContent);
-	table.setData(tableData);
-	for (const element of tableData) {
-		if (element.select) {
-			table.selectRow(element.id);
-		}
-	}
-	maxId = Math.max(...tableData.map(row => row.id));
-	bChanged = false;
-})
-
-document.getElementById("output-list").addEventListener("click", async function() {
-	const strStyle = document.getElementById("output-list-style").value;
-	let selectedRows = table.getSelectedData();
-	let strOut = "";
-	for (const element of selectedRows) {
-		strOut += strStyle.replace("$author$", element.author).replace("$name$", element.name).replace("$url$", element.url).replace("$nico$", element.niconico) + "\r\n";
-	}
-	const handle = await window.showSaveFilePicker({
-		suggestedName: "作品リスト",
-		types: [{
-			description: "Text file",
-			accept: {"text/plain": [".txt"]},
-		}],
-	});
-	const writable = await handle.createWritable();
-	await writable.write(strOut);
-	await writable.close();
-});
+document
+  .getElementById("output-list")
+  .addEventListener("click", async function () {
+    const strStyle = document.getElementById("output-list-style").value;
+    let selectedRows = table.getSelectedData();
+    let strOut = "";
+    for (const element of selectedRows) {
+      strOut +=
+        strStyle
+          .replace("$author$", element.author)
+          .replace("$name$", element.name)
+          .replace("$url$", element.url)
+          .replace("$nico$", element.niconico) + "\r\n";
+    }
+    const handle = await window.showSaveFilePicker({
+      suggestedName: "作品リスト",
+      types: [
+        {
+          description: "Text file",
+          accept: { "text/plain": [".txt"] },
+        },
+      ],
+    });
+    const writable = await handle.createWritable();
+    await writable.write(strOut);
+    await writable.close();
+  });
 
 window.addEventListener("beforeunload", alert_unsave);
